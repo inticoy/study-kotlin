e05fe3ef06c1234cf8122f1badfe425955131a1b
inticoy <inticoy0406@gmail.com>
2022-02-27 14:54:26 +0900

    Basic Syntax

--- src/basics/BasicSyntax.kt
+++ src/basics/BasicSyntax.kt
@@ -1,8 +1,26 @@
 package basics
 
+// function body can be an expression
 fun sum(x: Int, y: Int) = x + y
 
 fun main() {
     println(sum(1, 3))
+
+    // using ranges
+    for (x in 1..10 step 2) {
+        print(x)
+    }
+    println()
+    for (x in 9 downTo 0 step 3) {
+        print(x)
+    }
+    println()
+
+    val fruits = listOf("banana", "avocado", "apple", "kiwifruit")
+    fruits
+        .filter { it.startsWith("a") }
+        .sortedBy { it }
+        .map { it.uppercase() }
+        .forEach { println(it) }
 }
 
